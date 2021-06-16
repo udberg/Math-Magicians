cfa8e68f385e87d06fb73480923c1cd99aaff2a3
udberg <johnuduak066@gmail.com>
2021-06-16 14:15:22 +0100

    Add functionalities for button panel

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const nums = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
+const ops = ['/', 'x', '-', '+', '='];
+const topOps = ['AC', '+/-', '%'];
+
+const ButtonPanel = ({ handleClick }) => (
+    <div className="calculator">
+        <div className="nums-container">
+            <div className="top-btn">
+                {topOps.map((op) => (
+                    <Button
+                    btnName={op}
+                    handleClick={handleClick}
+                    className={'light-grey'}
+                    key={op}
+                    />
+                ))}
+                </div>
+                {nums.map(() => (
+                    <Button 
+                    btnName={num}
+                    handleClick={handleClick}
+                    className={`dark-grey ${num === '0' && 'big-h'}`}
+                    key={num}
+                    />
+                ))}
+
+                <Button
+                btnName={'.'}
+                handleClick={handleClick}
+                className={'light-grey'}
+                key={'.'}
+                />
+                </div>
+                <div className="ops-container">
+                    {ops.map((op) => (
+                        <Button 
+                        btnName={op}
+                        handleClick={handleClick}
+                        className={'orange'}
+                        key={op}
+                        />
+                    ))}
+                    </div>
+                    </div>
+);
+
+ButtonPanel.PropTypes = {
+    handleClick: PropTypes.func,
+};
+
+export default ButtonPanel;
