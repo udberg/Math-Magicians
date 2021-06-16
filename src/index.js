345be4b370e8615a6ec6c4a68099fd985cc4b91a
udberg <johnuduak066@gmail.com>
2021-06-16 14:56:05 +0100

    Refactor index.js to avoid conflict

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
-
-import ReactDom from 'react-dom';
-import App from './component/App';
-
-ReactDom.render(
-    <React.StrictMode>
-        <App />
-        </React.StrictMode>,
-        document.getElementById('root')
-);
-
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
