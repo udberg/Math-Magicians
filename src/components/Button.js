60ea5da75e4aeb94664890e6bc36ffaa77635d6e
udberg <johnuduak066@gmail.com>
2021-06-16 11:15:59 +0100

    Add app button

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = ({ btnName, handleClick, className }) => (
+   <button onClick={() => handleClick} className={className}>
+       {btnName}
+   </button> 
+);
+
+Button.PropTypes = {
+    btnName: PropTypes.string,
+    handleClick: PropTypes.func,
+    className: PropTypes.string,
+};
+
+export default Button;
