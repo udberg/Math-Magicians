a71f1a8d03b8c0c2baa995eea7bcdd3b3db31b62
udberg <johnuduak066@gmail.com>
2021-06-16 11:09:02 +0100

    Create App components and add display and display button

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
+const App = () => {
+    return (
+        <React.Fragment>
+        <Display total={'0'} />
+        <ButtonPanel handleClick={(f) => f} />
+        </React.Fragment>
+    );
+}
+
+export default App;
