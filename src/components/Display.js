887fca5bb53df64ac1a4a51fdaeaddb73c649411
udberg <johnuduak066@gmail.com>
2021-06-17 13:40:50 +0100

    Fix eslint problem

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Display = ({ total }) => <div className="display">{total || '0'}</div>;
 
 Display.propTypes = {
-  total: PropTypes.string.isRequired,
+  total: PropTypes.string,
 };
 
 export default Display;
