597230c04f8b4d929fc79ff53da9ab5e7ed5e86d
udberg <johnuduak066@gmail.com>
2021-06-22 08:38:00 +0100

    Initialize state with next, total and operation properties and make them clickable

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
+import calculate from '.../logic/calculate';
 
-const App = () => (
-  <>
-    <Display total="0" />
-    <ButtonPanel handleClick={(f) => f} />
-  </>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: '',
+      next: '',
+      operation: null,
+    };
+
+    this.handlClick = this.handleClick.bind(this);
+  }
+
+handlClick(btnName) {
+  const data = this.state;
+  const calc = calculate(data, btnName);
+  this.setState(()=>{
+    return {
+      ...calc
+    }
+  })
+}  
+render() {
+  const { total, next, operation } = this.setState;
+  return (
+    <div>
+      <display total={total} next={next} operation={operation} />
+      <ButtonPanel handlClick={this.handlClick} />
+    </div>
+  );
+}
+}
 
 export default App;
