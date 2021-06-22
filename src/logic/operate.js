336e2daf30f3c7cad6bb5447f071e283722d16bb
udberg <johnuduak066@gmail.com>
2021-06-22 08:23:00 +0100

    Make final operate changes

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,36 +1,36 @@
-import Big from "big.js";
+import Big from 'big.js';
 
 const operate = (number1, number2, operator) => {
-    if (!number1 || !number2 || !operator) return '0';
+  if (!number1 || !number2 || !operator) return '0';
 
-let res = 0;
-const num1 = 
-typeof number1 !== 'number' ? Big(Number(number1)) : Big(number1);
-const num2 =
-typeof number2 !== 'number' ? Big(Number(number2)) : Big(number2);
+  let res = 0;
+  const num1 =
+    typeof number1 !== 'number' ? Big(Number(number1)) : Big(number1);
+  const num2 =
+    typeof number2 !== 'number' ? Big(Number(number2)) : Big(number2);
 
-switch (operator) {
+  switch (operator) {
     case '+':
-        res = num1.plus(num2);
-        break;
+      res = num1.plus(num2);
+      break;
     case '-':
-        res = num1.minus(num2);
-        break;
+      res = num1.minus(num2);
+      break;
     case 'x':
-        res = num1.times(num2);
-        break;
+      res = num1.times(num2);
+      break;
     case '/':
-        res = num1.div(num2);
-        break;
+      res = num1.div(num2);
+      break;
     case '%':
-        res = num1.times(num2).div(100);
-        break;
+      res = num1.times(num2).div(100);
+      break;
     default:
-        res = 0;
-        break;
-}
+      res = 0;
+      break;
+  }
 
-return res.toString();
+  return res.toString();
 };
 
-export default operate;
+export default operate
