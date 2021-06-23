3c054a0e419482f4e43b5b5320992b49e03bbb21
udberg <johnuduak066@gmail.com>
2021-06-23 12:24:05 +0100

    Compute display functions

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ total }) => <div className="display">{total || '0'}</div>;
+const Display = ({ total, next, operation }) => (
+  <div className="display"> 
+  {total || '0'}
+  {operattion ? operation : ''}
+  {next}
+  </div>
+);
 
 Display.propTypes = {
   total: PropTypes.string.isRequired,
+  next: PropTypes.string.isRequired,
+  operation: PropTypes.string.isRequired,
 };
 
 export default Display;
