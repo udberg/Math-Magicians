9ccdd5f48b85a7aeeda13ad97e5aac5fc325a380
udberg <johnuduak066@gmail.com>
2021-07-01 09:14:21 +0100

    Use better indentations on test directory

--- src/__tests__/components/ButtonPanel.spec.js
+++ src/__tests__/components/ButtonPanel.spec.js
@@ -4,7 +4,7 @@ import '@testing-library/jest-dom';
 import ButtonPanel from '../../components/ButtonPanel';
 
 test('should render the button component', () => {
-render(<ButtonPanel />);
-const btnPanel = screen.getByTestId('button-panel');
-expect(btnPanel).toHaveTextContent('%');
+    render(<ButtonPanel />);
+    const btnPanel = screen.getByTestId('button-panel');
+    expect(btnPanel).toHaveTextContent('%');
 });
